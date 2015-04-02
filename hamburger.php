d640041624761424a81742e5be3fb472928ede4b
joeydsmith <joey@sparkexperience.com>
2015-04-02 14:20:49 -0400

    created codeine.io demo

--- hamburger.php
+++ hamburger.php
@@ -1,3 +1,4 @@
+<!-- DEMO: http://codepen.io/joeydsmith/pen/EaJpLK -->
 <div class="hamburglar">
 	<div class="burger-top"></div>
 	<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="6.4em" height="6.4em" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
